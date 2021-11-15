6683925dc502ba1bc833b4b3c5b4378cbbf14d2f
Jared <jaredblase@gmail.com>
2021-11-15 17:51:40 +0800

    refactor with backend server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
