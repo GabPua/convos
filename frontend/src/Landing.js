88560836ffa8835a865c2ac5440d31969e54cea5
Jared <jaredblase@gmail.com>
2021-11-17 07:23:20 +0800

    add: routing between landing and register

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -1,10 +1,12 @@
-function Landing() {
+import { Link } from 'react-router-dom'
+
+export default function Landing() {
   return (
     <div className="flex min-h-screen items-stretch">
       <div className="w-[51vw] m-auto flex-grow-[0.25] flex-shrink-0">
         <div className="flex flex-col items-center justify-center w-3/4 max-w-[500px] m-auto">
           <figure className="mb-3">
-            <img src="/logo-white.png" alt="Convos Logo" className="w-1/2 mb-5 m-auto" />
+            <img src="/logo-purple.png" alt="Convos Logo" className="w-1/2 mb-5 m-auto" />
             <figcaption className="text-[4vw] font-medium font-keep-calm text-center">CONVOS</figcaption>
           </figure>
           <div className="field w-2/3">
@@ -25,36 +27,11 @@ function Landing() {
             <img src="/speech-bubbles.png" alt="Speech bubbles" />
             <figcaption className="text-secondary font-keep-calm text-3xl text-center mt-4">Join the conversation!</figcaption>
           </figure>
-          <button className="btn primary w-1/2 text-xl mt-24">Register</button>
+          <Link to="/register" className="block w-full">
+            <button className="btn primary w-1/2 text-xl mt-24 m-auto">Register</button>
+          </Link>
         </div>
       </div>
     </div>
   );
 }
-
-function Register() {
-  return (
-    <div className="flex min-h-screen">
-      <div className="flex-grow-[2] bg-primary shadow-2xl">
-        <div className="flex flex-col items-center justify-center h-full">
-          <figure className="mb-5">
-            <img src="./logo.png" alt="Convos Logo" className="mb-10 m-auto" />
-            <figcaption className="text-8xl font-medium font-keep-calm text-secondary">CONVOS</figcaption>
-          </figure>
-          <button className="btn primary max-w-sm w-1/2 font-medium text-xl mt-3">Log In</button>
-        </div>
-      </div>
-      <div className="flex-grow">
-        <div className="flex flex-col items-center justify-center h-full w-5/6 m-auto pt-16 px-16">
-          <input type="text" className="input max-w-md w-full" placeholder="Email" />
-          <input type="text" className="input max-w-md w-full" placeholder="Username" />
-          <input type="text" className="input max-w-md w-full" placeholder="Password" />
-          <input type="text" className="input max-w-md w-full" placeholder="Confirm password" />
-          <button className="btn primary w-full max-w-md font-medium text-xl mt-24">Register</button>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default Landing;
