664b221e5726967cb8fcf59914b9af99d4f07316
Jared Sy <jaredblase@gmail.com>
2021-12-16 16:53:10 +0800

    use: authenticated routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
+import useAuth, { AuthProvider } from './utils/useAuth'
+import PropTypes from 'prop-types'
 import Landing from './routes/landing'
 import Register from './routes/register'
 import ResetForm from './routes/reset-form'
 import Dashboard from './routes/dashboard'
 import reportWebVitals from './reportWebVitals'
 
+function RequireAuth({ children }) {
+  const { isAuthed } = useAuth()
+  console.log('Is logged in: ', isAuthed())
+  return isAuthed() ? children : <Navigate to="/" replace />
+}
+
+RequireAuth.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+function RequireUnauth({ children }) {
+  const { isAuthed } = useAuth()
+  console.log('Is logged in: ', isAuthed())
+  return isAuthed() ? <Navigate to="/dashboard" replace /> : children
+}
+
+RequireUnauth.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="register" element={<Register />} />
-        <Route path="reset" element={<ResetForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <RequireUnauth>
+              <Landing />
+            </RequireUnauth>
+          } />
+          <Route path="register" element={
+            <RequireUnauth>
+              <Register />
+            </RequireUnauth>
+          } />
+          <Route path="reset" element={
+            <RequireUnauth>
+              <ResetForm />
+            </RequireUnauth>
+          } />
+          <Route path="/dashboard" element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
