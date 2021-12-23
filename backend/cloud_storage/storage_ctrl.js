a6e10045f2993c8c512648364d96d739831fbbe6
Jared Sy <jaredblase@gmail.com>
2021-12-23 14:02:14 +0800

    refactor: code cleanup

--- backend/cloud_storage/storage_ctrl.js
+++ backend/cloud_storage/storage_ctrl.js
@@ -5,23 +5,11 @@ module.exports = {
   getDetails: (req, res) => {
     res.json({
       cloudName: process.env.MEDIA_STORAGE_NAME,
-      apiKey: process.env.MEDIA_STORAGE_KEY
+      apiKey: process.env.MEDIA_STORAGE_KEY,
     });
   },
 
-  signRequest: (req, res) => {
-    console.log(req.body);
-
-    res.json({
-      signature: cloudinary.utils.api_sign_request(req.body, process.env.MEDIA_STORAGE_SECRET)
-    });
-  },
+  signRequest: (req, res) => res.json({
+    signature: cloudinary.utils.api_sign_request(req.body, process.env.MEDIA_STORAGE_SECRET)
+  }),
 };
-
-// res.json({
-//   signature: cloudinary.utils.api_sign_request({
-//     timestamp: timestamp,
-//     source: 'uw',
-//     folder: 'user_dps'
-//   }, process.env.MEDIA_STORAGE_SECRET)
-// });
