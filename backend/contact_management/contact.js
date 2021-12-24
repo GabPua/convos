1d477ab64cfdad02c53fc6fbf8744d72e97bc3bb
Jared Sy <jaredblase@gmail.com>
2021-12-25 01:20:54 +0800

    fix: spelling of required

--- backend/contact_management/contact.js
+++ backend/contact_management/contact.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 let contactSchema = new mongoose.Schema({
-  userId: { type: String, trim: true, lowercase: true, require: true, ref: 'User' },
-  contactId: { type: String, trim: true, lowercase: true, require: true, ref: 'User' },
+  userId: { type: String, trim: true, lowercase: true, required: true, ref: 'User' },
+  contactId: { type: String, trim: true, lowercase: true, required: true, ref: 'User' },
 });
 
 module.exports = mongoose.model('Contact', contactSchema, 'contacts');
