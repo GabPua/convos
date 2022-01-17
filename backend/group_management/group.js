baaae4b2170df028dae1b7d53e38e3f9791fb19b
GabPua <gabriel_pua@dlsu.edu.ph>
2022-01-17 11:46:54 +0800

    add: created mongoose schema for group

---
+++ backend/group_management/group.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// TODO: change default picture
+let groupSchema = new mongoose.Schema({
+  name: { type: String, trim: true, required: true },
+  picUri: { type: String, trim: true, required: true, default: 'https://res.cloudinary.com/convos456/image/upload/v1640260589/user_dps/avatar.png' },
+  admin: { type: String, trim: true, lowercase: true, required: true, ref: 'User' },
+  members: [{ type: String, trim: true, lowercase: true, required: true, ref: 'User' }],
+  tag: { type: String, trim: true, lowercase: true, required: true }
+});
+
+module.exports = mongoose.model('Groups', groupSchema, 'groups');
