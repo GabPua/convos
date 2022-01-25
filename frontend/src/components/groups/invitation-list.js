2ce495aeb87cd77f0c1a983da65de438d434b046
Jared Sy <jaredblase@gmail.com>
2022-01-25 18:15:58 +0800

    invitation ui

--- frontend/src/components/groups/invitation-list.js
+++ frontend/src/components/groups/invitation-list.js
@@ -7,14 +7,16 @@ function Invitation({ name, picUri }) {
     <div className="py-2 px-4 hover:bg-gray-200 group rounded-lg">
       <div className="flex justify-between transition-colors">
         <div className="flex items-center">
-          <img className="rounded-max w-12 mr-4" src={picUri} alt="Group Photo" />
+          <img className="rounded-full w-12 mr-4" src={picUri} alt="Group Photo" />
           <span className="text-xl">{name}</span>
         </div>
         <div className="flex items-center invisible group-hover:visible">
-          <span className="w-8 text-center mx-2">
-            <i className="fas fa-check fa-2x scale-90 text-green-600 cursor-pointer"></i></span>
-          <span className="w-8 text-center mx-2">
-            <i className="fas fa-times fa-2x scale-90 text-red-600 cursor-pointer"></i></span>
+          <span className="w-8 text-center mx-2 cursor-pointer">
+            <i className="fas fa-check fa-2x scale-90 text-green-600"></i>
+          </span>
+          <span className="w-8 text-center mx-2 cursor-pointer group-foo">
+            <i className="fas fa-times fa-2x scale-90 text-red-600"></i>
+          </span>
         </div>
       </div>
     </div>
@@ -24,6 +26,7 @@ function Invitation({ name, picUri }) {
 Invitation.propTypes = {
   name: PropTypes.string.isRequired,
   picUri: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 }
 
 export default function InvitationList() {
