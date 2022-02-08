50dfc50162624f91dbc0a67f062a9381464b53cd
MarioneDev <77104665+MarioneDev@users.noreply.github.com>
2022-02-09 03:58:34 +0800

    Revert "Merge branch 'dev' of https://github.com/GabPua/convos into dev"
    This reverts commit 9ff7d19ab394295ab8e1bc44f32128d9d6e2a793, reversing
    changes made to d026a273c3a81a045bd6d56929bbbbb9dc7f8762.

--- backend/test/group_management/removeMember.test.js
+++ backend/test/group_management/removeMember.test.js
@@ -4,7 +4,7 @@ const Member = require('../../group_management/member');
 const ctrl = require('../../group_management/group_ctrl');
 
 describe('Remove Member', () => {
-  let req, groupFindStub, memberFindStub, deleteStub, updateStub;
+  let req, groupFindStub, memberFindStub, deleteStub;
 
   beforeEach(() => {
     req = {
@@ -18,7 +18,6 @@ describe('Remove Member', () => {
     groupFindStub.restore();
     memberFindStub.restore();
     deleteStub.restore();
-    updateStub.restore();
   });
 
   it('encountered an error in removing member', (done) => {
@@ -28,18 +27,16 @@ describe('Remove Member', () => {
     
     const error = new Error();
     groupFindStub = sinon.stub(Group, 'findById').throws(error);
-    memberFindStub = sinon.stub(Member, 'find');
+    memberFindStub = sinon.stub(Member, 'findOne');
     deleteStub = sinon.stub(Member, 'deleteOne');
-    updateStub = sinon.stub(Group, 'updateOne');
 
     const res = {
       json: (result) => {
         try {
           expect(result.result).toBe(false);
           expect(result.error).toBe(error);
-          sinon.assert.notCalled(Member.find);
+          sinon.assert.notCalled(Member.findOne);
           sinon.assert.notCalled(Member.deleteOne);
-          sinon.assert.notCalled(Group.updateOne);
           done();
         } catch (error) {
           done(error);
@@ -57,39 +54,71 @@ describe('Remove Member', () => {
     req.params.id = '4eb6e7e7e9b7f4194e000001';
     req.session._id = 'admin@email.com';
     
-    const groupMock = {
+    const mock = {
       lean: () => {
         return {
           exec: () => { return null; }
         };
       }
     };
-    const memberMock = {
-      lean: () => {
-        return {
-          exec: () => { return []; }
-        };
-      }
-    };
     const member = {
       group: req.params.id,
-      user: {
-        $in: [req.body.userId, req.body.newAdmin]
-      }
+      user: req.body.userId
     };
-    groupFindStub = sinon.stub(Group, 'findById').returns(groupMock);
-    memberFindStub = sinon.stub(Member, 'find').returns(memberMock);
+    groupFindStub = sinon.stub(Group, 'findById').returns(mock);
+    memberFindStub = sinon.stub(Member, 'findOne').resolves(null);
     deleteStub = sinon.stub(Member, 'deleteOne');
-    updateStub = sinon.stub(Group, 'updateOne');
 
     const res = {
       json: (result) => {
         try {
           expect(result.result).toBe(false);
           expect(result.error.message).toBe('Unauthorized!');
-          sinon.assert.calledWith(Member.find, member);
+          sinon.assert.calledWith(Member.findOne, member);
           sinon.assert.notCalled(Member.deleteOne);
-          sinon.assert.notCalled(Group.updateOne);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      },
+
+      status: (result) => {
+        expect(result).toBe(401);
+      }
+    };
+
+    ctrl.removeMember(req, res);
+
+    sinon.assert.calledWith(Group.findById, req.params.id, 'admin');
+  });
+
+  it('is trying to remove the admin', (done) => {
+    req.body.userId = 'admin@email.com';
+    req.params.id = '4eb6e7e7e9b7f4194e000001';
+    req.session._id = 'admin@email.com';
+    
+    const mock = {
+      lean: () => {
+        return {
+          exec: () => { return { admin: 'admin@email.com' }; }
+        };
+      }
+    };
+    const member = {
+      group: req.params.id,
+      user: req.body.userId
+    };
+    groupFindStub = sinon.stub(Group, 'findById').returns(mock);
+    memberFindStub = sinon.stub(Member, 'findOne').resolves(true);
+    deleteStub = sinon.stub(Member, 'deleteOne');
+
+    const res = {
+      json: (result) => {
+        try {
+          expect(result.result).toBe(false);
+          expect(result.error.message).toBe('Unauthorized!');
+          sinon.assert.calledWith(Member.findOne, member);
+          sinon.assert.notCalled(Member.deleteOne);
           done();
         } catch (error) {
           done(error);
@@ -111,162 +140,36 @@ describe('Remove Member', () => {
     req.params.id = '4eb6e7e7e9b7f4194e000001';
     req.session._id = 'member@email.com';
     
-    const groupMock = {
-      lean: () => {
-        return {
-          exec: () => { return { admin: 'admin@email.com' }; }
-        };
-      }
-    };
-    const memberMock = {
-      lean: () => {
-        return {
-          exec: () => { return [{ user: req.body.userId }]; }
-        };
-      }
-    };
-    const member = {
-      group: req.params.id,
-      user: {
-        $in: [req.body.userId, req.body.newAdmin]
-      }
-    };
-    groupFindStub = sinon.stub(Group, 'findById').returns(groupMock);
-    memberFindStub = sinon.stub(Member, 'find').returns(memberMock);
-    deleteStub = sinon.stub(Member, 'deleteOne');
-    updateStub = sinon.stub(Group, 'updateOne');
-
-    const res = {
-      json: (result) => {
-        try {
-          expect(result.result).toBe(false);
-          expect(result.error.message).toBe('Unauthorized!');
-          sinon.assert.calledWith(Member.find, member);
-          sinon.assert.notCalled(Member.deleteOne);
-          sinon.assert.notCalled(Group.updateOne);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      },
-
-      status: (result) => {
-        expect(result).toBe(401);
-      }
-    };
-
-    ctrl.removeMember(req, res);
-
-    sinon.assert.calledWith(Group.findById, req.params.id, 'admin');
-  });
-
-  it('is trying to remove the admin without a new admin', (done) => {
-    req.body.userId = 'admin@email.com';
-    req.params.id = '4eb6e7e7e9b7f4194e000001';
-    req.session._id = 'admin@email.com';
-    
-    const groupMock = {
-      lean: () => {
-        return {
-          exec: () => { return { admin: 'admin@email.com' }; }
-        };
-      }
-    };
-    const memberMock = {
-      lean: () => {
-        return {
-          exec: () => { return [{ user: req.body.userId }]; }
-        };
-      }
-    };
-    const member = {
-      group: req.params.id,
-      user: {
-        $in: [req.body.userId, req.body.newAdmin]
-      }
-    };
-    groupFindStub = sinon.stub(Group, 'findById').returns(groupMock);
-    memberFindStub = sinon.stub(Member, 'find').returns(memberMock);
-    deleteStub = sinon.stub(Member, 'deleteOne');
-    updateStub = sinon.stub(Group, 'updateOne');
-
-    const res = {
-      json: (result) => {
-        try {
-          expect(result.result).toBe(false);
-          expect(result.error.message).toBe('Unauthorized!');
-          sinon.assert.calledWith(Member.find, member);
-          sinon.assert.notCalled(Member.deleteOne);
-          sinon.assert.notCalled(Group.updateOne);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      },
-
-      status: (result) => {
-        expect(result).toBe(401);
-      }
-    };
-
-    ctrl.removeMember(req, res);
-
-    sinon.assert.calledWith(Group.findById, req.params.id, 'admin');
-  });
-
-  it('is trying to remove the admin with a new admin', (done) => {
-    req.body.userId = 'admin@email.com';
-    req.body.newAdmin = 'next@email.com';
-    req.params.id = '4eb6e7e7e9b7f4194e000001';
-    req.session._id = 'admin@email.com';
-    
-    const groupMock = {
-      lean: () => {
-        return {
-          exec: () => { return { admin: 'admin@email.com' }; }
-        };
-      }
-    };
-    const memberMock = {
-      lean: () => {
-        return {
-          exec: () => { return [{ user: req.body.userId }, { user: req.body.newAdmin }]; }
-        };
-      }
-    };
     const mock = {
       lean: () => {
         return {
-          exec: () => { return null; }
+          exec: () => { return { admin: 'admin@email.com' }; }
         };
       }
     };
     const member = {
       group: req.params.id,
-      user: {
-        $in: [req.body.userId, req.body.newAdmin]
-      }
-    };
-    const deleteMember = {
-      group: req.params.id,
       user: req.body.userId
     };
-    groupFindStub = sinon.stub(Group, 'findById').returns(groupMock);
-    memberFindStub = sinon.stub(Member, 'find').returns(memberMock);
-    deleteStub = sinon.stub(Member, 'deleteOne').resolves();
-    updateStub = sinon.stub(Group, 'updateOne').returns(mock);
+    groupFindStub = sinon.stub(Group, 'findById').returns(mock);
+    memberFindStub = sinon.stub(Member, 'findOne').resolves(true);
+    deleteStub = sinon.stub(Member, 'deleteOne');
 
     const res = {
       json: (result) => {
         try {
-          expect(result.result).toBe(true);
-          sinon.assert.calledWith(Member.find, member);
-          sinon.assert.calledWith(Member.deleteOne, deleteMember);
-          sinon.assert.calledWith(Group.updateOne, { _id: req.params.id }, { admin: req.body.newAdmin });
+          expect(result.result).toBe(false);
+          expect(result.error.message).toBe('Unauthorized!');
+          sinon.assert.calledWith(Member.findOne, member);
+          sinon.assert.notCalled(Member.deleteOne);
           done();
         } catch (error) {
           done(error);
         }
+      },
+
+      status: (result) => {
+        expect(result).toBe(401);
       }
     };
 
@@ -280,42 +183,27 @@ describe('Remove Member', () => {
     req.params.id = '4eb6e7e7e9b7f4194e000001';
     req.session._id = 'admin@email.com';
     
-    const groupMock = {
+    const mock = {
       lean: () => {
         return {
           exec: () => { return { admin: 'admin@email.com' }; }
         };
       }
     };
-    const memberMock = {
-      lean: () => {
-        return {
-          exec: () => { return [{ user: req.body.userId }]; }
-        };
-      }
-    };
     const member = {
-      group: req.params.id,
-      user: {
-        $in: [req.body.userId, req.body.newAdmin]
-      }
-    };
-    const deleteMember = {
       group: req.params.id,
       user: req.body.userId
     };
-    groupFindStub = sinon.stub(Group, 'findById').returns(groupMock);
-    memberFindStub = sinon.stub(Member, 'find').returns(memberMock);
+    groupFindStub = sinon.stub(Group, 'findById').returns(mock);
+    memberFindStub = sinon.stub(Member, 'findOne').resolves(true);
     deleteStub = sinon.stub(Member, 'deleteOne').resolves();
-    updateStub = sinon.stub(Group, 'updateOne');
 
     const res = {
       json: (result) => {
         try {
           expect(result.result).toBe(true);
-          sinon.assert.calledWith(Member.find, member);
-          sinon.assert.calledWith(Member.deleteOne, deleteMember);
-          sinon.assert.notCalled(Group.updateOne);
+          sinon.assert.calledWith(Member.findOne, member);
+          sinon.assert.calledWith(Member.deleteOne, member);
           done();
         } catch (error) {
           done(error);
@@ -333,42 +221,27 @@ describe('Remove Member', () => {
     req.params.id = '4eb6e7e7e9b7f4194e000001';
     req.session._id = 'member@email.com';
     
-    const groupMock = {
+    const mock = {
       lean: () => {
         return {
           exec: () => { return { admin: 'admin@email.com' }; }
         };
       }
     };
-    const memberMock = {
-      lean: () => {
-        return {
-          exec: () => { return [{ user: req.body.userId }]; }
-        };
-      }
-    };
     const member = {
-      group: req.params.id,
-      user: {
-        $in: [req.body.userId, req.body.newAdmin]
-      }
-    };
-    const deleteMember = {
       group: req.params.id,
       user: req.body.userId
     };
-    groupFindStub = sinon.stub(Group, 'findById').returns(groupMock);
-    memberFindStub = sinon.stub(Member, 'find').returns(memberMock);
+    groupFindStub = sinon.stub(Group, 'findById').returns(mock);
+    memberFindStub = sinon.stub(Member, 'findOne').resolves(true);
     deleteStub = sinon.stub(Member, 'deleteOne').resolves();
-    updateStub = sinon.stub(Group, 'updateOne');
 
     const res = {
       json: (result) => {
         try {
           expect(result.result).toBe(true);
-          sinon.assert.calledWith(Member.find, member);
-          sinon.assert.calledWith(Member.deleteOne, deleteMember);
-          sinon.assert.notCalled(Group.updateOne);
+          sinon.assert.calledWith(Member.findOne, member);
+          sinon.assert.calledWith(Member.deleteOne, member);
           done();
         } catch (error) {
           done(error);
